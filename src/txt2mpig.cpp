cf4735b16928f5cda3e6e7ce64cc78efca23ff02
Michael Yantosca <mike@archivarius.net>
2019-04-17 23:30:05 -0500

    Fix issues with multiple process runs of txt2mpig.

--- src/txt2mpig.cpp
+++ src/txt2mpig.cpp
@@ -43,6 +43,7 @@ int main(int argc, char *argv[]) {
 
   FILE* fp_in = NULL;
   MPI_File mpi_fp_out;
+  size_t mpi_fp_out_off = 0;
 
   size_t edge_wr_buf_sz = 2 * edges_per_block;
   size_t edge_rd_buf_sz = edge_wr_buf_sz * machines;
@@ -66,73 +67,89 @@ int main(int argc, char *argv[]) {
   }
 
   MPI_File_open(MPI_COMM_WORLD, fname_out, MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &mpi_fp_out);
-  //MPI_File_set_view(mpi_fp_out, 0, MPI_UNSIGNED, MPI_UNSIGNED, "native", MPI_INFO_NULL);
+  MPI_File_set_view(mpi_fp_out, 0, MPI_UNSIGNED, MPI_UNSIGNED, "native", MPI_INFO_NULL);
+  // Read buffer offset.
   size_t edge_rd_buf_off = 0;
-  size_t mpi_fp_out_off = 0;
-  int c = 0;
+
+  // Temporary variable for line of text.
+  char *line = NULL;
+  // Input text line size.
+  size_t line_sz = 0;
+
+  MPI_Request dump_req = MPI_REQUEST_NULL;
   // Iterate over input text file.
   while (!eof) {
     if (rank == 0) {
       uint32_t u = 0;
       uint32_t v = 0;
-      int found = fscanf(fp_in, "%u %u", &u, &v);
+      // Read next line from input file.
+      getline(&line, &line_sz, fp_in);
+      // Check for two unsigned numbers separated by whitespace
+      // representing the endpoints of the edge in the retrieved
+      // line of text.
+      int found = sscanf(line, "%u %u", &u, &v);
       if (found == 2) {
-	// Line contains exactly two unsigned numbers
-	// representing the endpoints of the edge.
 	edge_rd_buf[edge_rd_buf_off++] = u;
 	edge_rd_buf[edge_rd_buf_off++] = v;
-	//fprintf(stderr, "%d: (%u, %u)\n", c++, u, v);
       }
-      else {
-	fscanf(fp_in, "%*s\n");
-      }
-      if (edge_rd_buf_off == edge_rd_buf_sz) {
-	MPI_Bcast(&edge_rd_buf_off, 1, MPI_INT, 0, MPI_COMM_WORLD);
-      }
-      if (eof = feof(fp_in)) {
-	MPI_Bcast(&eof, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	int last_machine = edge_rd_buf_off / (machines * edge_wr_buf_sz) + 1;
-	for (int machine = last_machine + 1; machine < machines; machine++) {
-	  edge_wr_buf_displs[machine] = 0;
-	  edge_wr_buf_counts[machine] = 0;
-	}
-	if (last_machine < machines) {
-	  edge_wr_buf_counts[last_machine] = edge_rd_buf_off % (machines * edge_wr_buf_sz);
-	}
-      }
+      eof = feof(fp_in);
     }
+
+    // Update all other processes on read buffer offset.
+    MPI_Bcast(&edge_rd_buf_off, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
+    // Update all other processes on EOF status.
+    MPI_Bcast(&eof, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Check whether read buffer is full or EOF.
     if ((edge_rd_buf_off == edge_rd_buf_sz) || eof) {
-      int rcvd = 0;
-      MPI_Scatter(edge_wr_buf_counts, 1, MPI_UNSIGNED,
-		  &rcvd, 1, MPI_UNSIGNED,
-		  0, MPI_COMM_WORLD);
-      // Dump buffer to disk.
-      MPI_Scatterv(edge_rd_buf, edge_wr_buf_counts, edge_wr_buf_displs, MPI_UNSIGNED,
-		   edge_wr_buf, edge_wr_buf_sz, MPI_UNSIGNED,
-		   0, MPI_COMM_WORLD);
-      if (rcvd > 0) {
-	// @FIXME: mpirun -np > 1 hangs.
-	MPI_File_set_view(mpi_fp_out, (mpi_fp_out_off + rank * edge_wr_buf_sz) * sizeof(uint32_t),
-			  MPI_UNSIGNED, MPI_UNSIGNED, "native", MPI_INFO_NULL);
-	MPI_Request dump_req;
-	MPI_File_iwrite_at(mpi_fp_out, 0, edge_wr_buf, rcvd, MPI_UNSIGNED, &dump_req);
-	MPI_Wait(&dump_req, NULL);
+      // Wait on file dump before overwriting the write buffer.
+      MPI_Wait(&dump_req, NULL);
+      // Reset dump request to avoid segfault.
+      dump_req = MPI_REQUEST_NULL;
+      // Divide the input among all the processes.
+      MPI_Scatter(edge_rd_buf, edge_wr_buf_sz, MPI_UNSIGNED,
+      		  edge_wr_buf, edge_wr_buf_sz, MPI_UNSIGNED,
+      		  0, MPI_COMM_WORLD);
+      // Number of processes participating in the actual write.
+      int participants = edge_rd_buf_off / edge_wr_buf_sz;
+      // Buffer size to be written by a given process.
+      int edge_wr_buf_count =
+	(rank < participants                     // rank well below the offset cutoff
+	 ? edge_wr_buf_sz                        //   Write away!
+	 : (rank == participants                 // rank on the cutoff border
+	    ? edge_rd_buf_off % edge_wr_buf_sz   //   If remainder exists, write that.
+	    : 0));                               //   Otherwise, nothing.
+      // Only write if necessary.
+      if (edge_wr_buf_count > 0) {
+	MPI_File_iwrite_at(mpi_fp_out, mpi_fp_out_off + rank * edge_wr_buf_sz,
+			   edge_wr_buf, edge_wr_buf_count, MPI_UNSIGNED, &dump_req);
       }
 
+      // Increment absolute write offset.
       mpi_fp_out_off += edge_rd_buf_off;
+      // Reset read buffer offset.
       edge_rd_buf_off = 0;
     }
 
   }
 
-  // Close file handles.
+  // Wait on file dump, just in case.
+  MPI_Wait(&dump_req, NULL);
+
+  // Clean up.
   if (rank == 0) {
     fclose(fp_in);
+    delete[] edge_rd_buf;
+    delete[] edge_wr_buf_counts;
+    delete[] edge_wr_buf_displs;
   }
+
+  delete[] edge_wr_buf;
   MPI_File_close(&mpi_fp_out);
+
   // Tear down MPI.
   MPI_Finalize();
+
+  // Exit.
   return 0;
 }
