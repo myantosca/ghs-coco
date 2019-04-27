84318513815fef98dabb8cdc9ad39b810d6fac5d
Michael Yantosca <mike@archivarius.net>
2019-04-27 00:27:24 -0500

    Adjusted order of k values since k=32 is hit or miss on crill, even with constraints.

--- bfs-coco.sh
+++ bfs-coco.sh
@@ -8,7 +8,7 @@ export TS="$(date +%Y-%m-%d.%H.%M.%S)"
 export DOUT="./results/bfs-coco/${TS}"
 mkdir -p ${DOUT}
 
-for ((k = 32; k > 0; k /= 2)); do
+for ((k = 1; k <= 32; k *= 2)); do
     for fname in $(find ./graphs -name '*.ecg'); do
 	bname=$(basename $fname)
 	mpirun -np $k ./build/bfs-coco -i $fname 1> ${DOUT}/bfs-coco.$k.$bname.out 2> /dev/null
