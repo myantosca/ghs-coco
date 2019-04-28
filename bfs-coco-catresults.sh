5e559796acecaf39297d5016bf5aa1c28ae627d3
Michael Yantosca <mike@archivarius.net>
2019-04-28 12:45:14 -0500

    Fix concatentation script to pivot on k.

--- bfs-coco-catresults.sh
+++ bfs-coco-catresults.sh
@@ -3,12 +3,12 @@
 ROOT=$1
 
 for g in "vg" "mc"; do
-    for ((n = 10; n <= 20; n++)); do
-	find $ROOT -name bfs-coco.*.$g.$n.1-.* | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.$g.$n.1-.csv
-	find $ROOT -name bfs-coco.*.$g.$n.1.*  | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.$g.$n.1.csv
-	find $ROOT -name bfs-coco.*.$g.$n.1+.* | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.$g.$n.1+.csv
-	find $ROOT -name bfs-coco.*.$g.$n.L-.* | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.$g.$n.L-.csv
-	find $ROOT -name bfs-coco.*.$g.$n.L.*  | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.$g.$n.L.csv
-	find $ROOT -name bfs-coco.*.$g.$n.L+.* | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.$g.$n.L+.csv
+    for ((k = 1; k <= 32; k*=2)); do
+	find $ROOT -name bfs-coco.$k.$g.*.1-.* | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.k$k.$g.1-.csv
+	find $ROOT -name bfs-coco.$k.$g.*.1.*  | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.k$k.$g.1.csv
+	find $ROOT -name bfs-coco.$k.$g.*.1+.* | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.k$k.$g.1+.csv
+	find $ROOT -name bfs-coco.$k.$g.*.L-.* | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.k$k.$g.L-.csv
+	find $ROOT -name bfs-coco.$k.$g.*.L.*  | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.k$k.$g.L.csv
+	find $ROOT -name bfs-coco.$k.$g.*.L+.* | xargs head -2q | sed '3~2d' | sort -n -t , -k 1,2 > $ROOT/bfs-coco.k$k.$g.L+.csv
     done
 done
